1d6f204e905ac3ee36f09fa1a85a5c477c42ec13
Composition Fore <composition4project@gmail.com>
2017-09-16 03:12:26 -0400

    change var to this.osc_abssin etc.

--- sources/scripts/core/oscs.js
+++ sources/scripts/core/oscs.js
@@ -2,31 +2,31 @@
 var Oscs = function () {
     // OSCs
 
-    var osc_sin = function (value) {
+    this.osc_sin = function (value) {
         return Math.sin(value * 2 * Math.PI);
     };
 
-    var osc_abssin = function (value) {
+    this.osc_abssin = function (value) {
         return 0.5 + Math.sin(value * 2 * Math.PI);
     }
 
-    var osc_saw = function (value) {
+    this.osc_saw = function (value) {
         return 2 * (value % 1) - 1;
     };
 
-    var osc_revsaw = function (value) {
+    this.osc_revsaw = function (value) {
         return 2 * (1 - (value % 1));
     }
 
-    var osc_square = function (value) {
+    this.osc_square = function (value) {
         return (value % 1) < 0.5 ? 1 : -1;
     };
 
-    var osc_noise = function (value) {
+    this.osc_noise = function (value) {
         return (2 * Math.random() - 1);
     }
 
-    var osc_tri = function (value) {
+    this.osc_tri = function (value) {
         var v2 = (value % 1) * 4;
         if (v2 < 2) return v2 - 1;
         return 3 - v2;
