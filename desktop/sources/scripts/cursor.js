c43d6be043c7e89f3cddf372438f0068931974e2
Devine Lu Linvega <aliceffekt@gmail.com>
2019-05-04 10:20:54 +0900

    Added track movement

--- desktop/sources/scripts/cursor.js
+++ desktop/sources/scripts/cursor.js
@@ -1,6 +1,7 @@
 'use strict'
 
 function Cursor (terminal) {
+  this.track = 0
   this.pos = { x: 0, y: 0 }
 
   this.install = function (host = document.body) {
@@ -10,7 +11,18 @@ function Cursor (terminal) {
   }
 
   this.move = function (x = 0, y = 0) {
-    this.pos = { x: clamp(this.pos.x + x, 0, 16), y: clamp(this.pos.y + y, 0, 16) }
+    // Change track
+    if(this.pos.y === 15 && y > 0){
+      this.pos = { x: this.pos.x, y: 0 }  
+      this.track = clamp(this.track + 1,0,15)
+    }
+    else if(this.pos.y === 0 && y < 0 && this.track >= 1){
+      this.pos = { x: this.pos.x, y: 15 }  
+      this.track = clamp(this.track - 1,0,15)
+    }
+    else{
+      this.pos = { x: clamp(this.pos.x + x, 0, 15), y: clamp(this.pos.y + y, 0, 15) }  
+    }
     terminal.update()
   }
 
